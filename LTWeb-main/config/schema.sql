707681227a1bb0cdfae94ab8f487f33c0b66557d
sheng69 <thaitai2k5@gmail.com>
2026-09-15 15:51:43 +0700

    Thai_Tai:Update xong trailer,thumbnail,suất chiếu

--- LTWeb-main/config/schema.sql
+++ LTWeb-main/config/schema.sql
@@ -88,6 +88,27 @@ INSERT INTO `movies` (`id`, `title`, `director`, `cast`, `genre`, `duration`, `r
 (19, 'NGƯỜI VỢ CUỐI CÙNG', 'Victor Vũ', 'Kaity Nguyễn, Quang Thắng', 'Tâm lý, Cổ trang', 130, '2023-11-03', 'nguoi-vo-cuoi-cung.jpg', 'Bi kịch tình yêu của người vợ ba trong một gia đình quan lại thời phong kiến.', 'now_showing'),
 (20, 'FURIOSA: MỘT SỬ THI MAD MAX', 'George Miller', 'Anya Taylor-Joy, Chris Hemsworth', 'Hành động, Viễn tưởng', 148, '2024-05-24', 'furiosa.jpg', 'Nguồn gốc của Furiosa trước khi trở thành chiến binh huyền thoại trong thế giới hậu tận thế.', 'coming_soon');
 
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=d1ZHdosjNX8' WHERE id = 1;  -- LẬT MẶT 7: MỘT ĐIỀU ƯỚC
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=Xithigfg7dA' WHERE id = 2;  -- DEADPOOL
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=IkaP0KJWTsQ' WHERE id = 3;  -- NHÀ BÀ NỮ
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=jluSu8Rw6YE&pp=ygURYuG7kSBnacOgIHRyYWlsZXI%3D' WHERE id = 4;  -- BỐ GIÀ
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=qn1t_biQigc&pp=ygUdxJHDoG8gcGjhu58gdsOgIHBpYW5vIHRyYWlsZXI%3D' WHERE id = 5;  -- ĐÀO, PHỞ VÀ PIANO
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=66K9-l0EkE0' WHERE id = 6;  -- EXHUMA: QUẬT MỘ TRÙNG MA
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=5XkgG_AAQs0&pp=ygUpZ29kemlsbGEgeCBrb25nIMSR4bq_IGNo4bq_IG3hu5tpIHRyYWlsZXLSBwkJJAwBhyohjO8%3D' WHERE id = 7;  -- GODZILLA X KONG: ĐẾ CHẾ MỚI
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=_inKs4eeHiI&pp=ygUXa3VuZyBmdSBwYW5kYSA0IHRyYWlsZXI%3D' WHERE id = 8;  -- KUNG FU PANDA 4
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=LEjhY15eCx0&pp=ygUUaW5zaWRlIG91dCAyIHRyYWlsZXI%3D' WHERE id = 9;  -- INSIDE OUT 2
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 10; -- DEADPOOL & WOLVERINE
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=xWh0g4rKGjI&pp=ygUWa-G6uyDEg24gaOG7k24gdHJhaWxlcg%3D%3D' WHERE id = 11; -- KẺ ĂN HỒN
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 12; -- MÓNG VUỐT
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=79BznZKQwIQ&pp=ygUkY8OhaSBnacOhIGPhu6dhIGjhuqFuaCBwaMO6YyB0cmFpbGVy' WHERE id = 13; -- CÁI GIÁ CỦA HẠNH PHÚC
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 14; -- DUNE: HÀNH TINH CÁT 2
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 15; -- WONKA
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 16; -- AQUAMAN VÀ VƯƠNG QUỐC THẤT LẠC
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 17; -- CHỊ CHỊ EM EM 2
+UPDATE movies SET trailer_url = 'https://www.youtube.com/watch?v=hktzirCnJmQ&pp=ygUixJHhuqV0IHLhu6tuZyBwaMawxqFuZyBuYW0gdHJhaWxlcg%3D%3D' WHERE id = 18; -- ĐẤT RỪNG PHƯƠNG NAM
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 19; -- NGƯỜI VỢ CUỐI CÙNG
+UPDATE movies SET trailer_url = 'DÁN_LINK_YOUTUBE_VÀO_ĐÂY' WHERE id = 20; -- FURIOSA: MỘT SỬ THI MAD MAX
+
 INSERT INTO `cinemas` (`id`, `name`, `address`) VALUES
 (1, 'CineStar Quốc Thanh', '271 Nguyễn Trãi, Q.1, TP.HCM'),
 (2, 'CineStar Hai Bà Trưng', '135 Hai Bà Trưng, Q.3, TP.HCM');
@@ -101,51 +122,83 @@ INSERT INTO `cinemas` (`id`, `name`, `address`) VALUES
 (10, 'CGV Vincom Bà Triệu', 'Vincom Center Bà Triệu, 191 Bà Triệu, Hai Bà Trưng, Hà Nội');
 
 INSERT INTO `showtimes` (`id`, `movie_id`, `cinema_id`, `show_date`, `show_time`, `price`) VALUES
-(1, 1, 1, CURDATE(), '10:30:00', 65000),
-(2, 1, 1, CURDATE(), '19:30:00', 95000),
-(3, 1, 1, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(4, 1, 1, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(5, 1, 3, CURDATE(), '10:30:00', 65000),
-(6, 1, 3, CURDATE(), '19:30:00', 95000),
-(7, 1, 3, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(8, 1, 3, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(9, 2, 1, CURDATE(), '10:30:00', 65000),
-(10, 2, 1, CURDATE(), '19:30:00', 95000),
-(11, 2, 1, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(12, 2, 1, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(13, 2, 3, CURDATE(), '10:30:00', 65000),
-(14, 2, 3, CURDATE(), '19:30:00', 95000),
-(15, 2, 3, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(16, 2, 3, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(17, 6, 1, CURDATE(), '10:30:00', 65000),
-(18, 6, 1, CURDATE(), '19:30:00', 95000),
-(19, 6, 1, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(20, 6, 1, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(21, 6, 3, CURDATE(), '10:30:00', 65000),
-(22, 6, 3, CURDATE(), '19:30:00', 95000),
-(23, 6, 3, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(24, 6, 3, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(25, 7, 1, CURDATE(), '10:30:00', 65000),
-(26, 7, 1, CURDATE(), '19:30:00', 95000),
-(27, 7, 1, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(28, 7, 1, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(29, 7, 3, CURDATE(), '10:30:00', 65000),
-(30, 7, 3, CURDATE(), '19:30:00', 95000),
-(31, 7, 3, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(32, 7, 3, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(33, 12, 1, CURDATE(), '10:30:00', 65000),
-(34, 12, 1, CURDATE(), '19:30:00', 95000),
-(35, 12, 1, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(36, 12, 1, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(37, 12, 3, CURDATE(), '10:30:00', 65000),
-(38, 12, 3, CURDATE(), '19:30:00', 95000),
-(39, 12, 3, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(40, 12, 3, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(41, 14, 1, CURDATE(), '10:30:00', 65000),
-(42, 14, 1, CURDATE(), '19:30:00', 95000),
-(43, 14, 1, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(44, 14, 1, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000),
-(45, 14, 3, CURDATE(), '10:30:00', 65000),
-(46, 14, 3, CURDATE(), '19:30:00', 95000),
-(47, 14, 3, CURDATE() + INTERVAL 1 DAY, '10:30:00', 65000),
-(48, 14, 3, CURDATE() + INTERVAL 1 DAY, '19:30:00', 95000);
+(1, 1, 1, CURDATE(), '09:30:00', 55000),
+(2, 1, 2, CURDATE(), '13:00:00', 65000),
+(3, 1, 3, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(4, 1, 4, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(5, 2, 5, CURDATE(), '09:30:00', 55000),
+(6, 2, 6, CURDATE(), '13:00:00', 65000),
+(7, 2, 7, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(8, 2, 8, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(9, 3, 9, CURDATE(), '09:30:00', 55000),
+(10, 3, 10, CURDATE(), '13:00:00', 65000),
+(11, 3, 1, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(12, 3, 2, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(13, 4, 3, CURDATE(), '09:30:00', 55000),
+(14, 4, 4, CURDATE(), '13:00:00', 65000),
+(15, 4, 5, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(16, 4, 6, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(17, 5, 7, CURDATE(), '09:30:00', 55000),
+(18, 5, 8, CURDATE(), '13:00:00', 65000),
+(19, 5, 9, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(20, 5, 10, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(21, 6, 1, CURDATE(), '09:30:00', 55000),
+(22, 6, 2, CURDATE(), '13:00:00', 65000),
+(23, 6, 3, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(24, 6, 4, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(25, 7, 5, CURDATE(), '09:30:00', 55000),
+(26, 7, 6, CURDATE(), '13:00:00', 65000),
+(27, 7, 7, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(28, 7, 8, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(29, 8, 9, CURDATE(), '09:30:00', 55000),
+(30, 8, 10, CURDATE(), '13:00:00', 65000),
+(31, 8, 1, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(32, 8, 2, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(33, 9, 3, CURDATE(), '09:30:00', 55000),
+(34, 9, 4, CURDATE(), '13:00:00', 65000),
+(35, 9, 5, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(36, 9, 6, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(37, 10, 7, CURDATE(), '09:30:00', 55000),
+(38, 10, 8, CURDATE(), '13:00:00', 65000),
+(39, 10, 9, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(40, 10, 10, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(41, 11, 1, CURDATE(), '09:30:00', 55000),
+(42, 11, 2, CURDATE(), '13:00:00', 65000),
+(43, 11, 3, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(44, 11, 4, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(45, 12, 5, CURDATE(), '09:30:00', 55000),
+(46, 12, 6, CURDATE(), '13:00:00', 65000),
+(47, 12, 7, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(48, 12, 8, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(49, 13, 9, CURDATE(), '09:30:00', 55000),
+(50, 13, 10, CURDATE(), '13:00:00', 65000),
+(51, 13, 1, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(52, 13, 2, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(53, 14, 3, CURDATE(), '09:30:00', 55000),
+(54, 14, 4, CURDATE(), '13:00:00', 65000),
+(55, 14, 5, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(56, 14, 6, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(57, 15, 7, CURDATE(), '09:30:00', 55000),
+(58, 15, 8, CURDATE(), '13:00:00', 65000),
+(59, 15, 9, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(60, 15, 10, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(61, 16, 1, CURDATE(), '09:30:00', 55000),
+(62, 16, 2, CURDATE(), '13:00:00', 65000),
+(63, 16, 3, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(64, 16, 4, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(65, 17, 5, CURDATE(), '09:30:00', 55000),
+(66, 17, 6, CURDATE(), '13:00:00', 65000),
+(67, 17, 7, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(68, 17, 8, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(69, 18, 9, CURDATE(), '09:30:00', 55000),
+(70, 18, 10, CURDATE(), '13:00:00', 65000),
+(71, 18, 1, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(72, 18, 2, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(73, 19, 3, CURDATE(), '09:30:00', 55000),
+(74, 19, 4, CURDATE(), '13:00:00', 65000),
+(75, 19, 5, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(76, 19, 6, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000),
+(77, 20, 7, CURDATE(), '09:30:00', 55000),
+(78, 20, 8, CURDATE(), '13:00:00', 65000),
+(79, 20, 9, CURDATE() + INTERVAL 1 DAY, '17:15:00', 85000),
+(80, 20, 10, CURDATE() + INTERVAL 1 DAY, '20:30:00', 95000);
